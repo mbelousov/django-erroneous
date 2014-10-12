7cd2b42e5e870ad6788bad8db0eb5deed9fe2270
sgs00 <g.saviano@gmail.com>
2014-10-13 00:51:53 +0200

    pep8

--- erroneous/models.py
+++ erroneous/models.py
@@ -1,28 +1,17 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 class Error(models.Model):
     """
     Model for storing the individual errors.
     """
-    kind = models.CharField(_('type'),
-        null=True, blank=True, max_length=128, db_index=True
-    )
-    info = models.TextField(
-        null=False,
-    )
-    data = models.TextField(
-        blank=True, null=True
-    )
-    path = models.URLField(
-        null=True, blank=True,
-    )
-    when = models.DateTimeField(
-        null=False, auto_now_add=True, db_index=True,
-    )
-    html = models.TextField(
-        null=True, blank=True,
-    )
+    kind = models.CharField(_('type'), null=True, blank=True, max_length=128, db_index=True)
+    info = models.TextField(null=False)
+    data = models.TextField(blank=True, null=True)
+    path = models.URLField(null=True, blank=True)
+    when = models.DateTimeField(null=False, auto_now_add=True, db_index=True)
+    html = models.TextField(null=True, blank=True)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
